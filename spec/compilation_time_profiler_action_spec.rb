8840b4c3e0fe89d407b0fe5355560f2b6c8338be
Yuta Saito <kateinoigakukun@gmail.com>
2019-06-11 23:58:01 +0900

    Update

--- spec/compilation_time_profiler_action_spec.rb
+++ spec/compilation_time_profiler_action_spec.rb
@@ -1,11 +1,10 @@
 describe Fastlane::Actions::CompilationTimeProfilerAction do
   describe '#run' do
     it '#run' do
-      require 'tempfile'
       fixture = File.expand_path('../fixtures', __FILE__)
-      allow(Dir).to receive(:mktmpdir).and_return("foo/bar")
+      allow(Dir).to receive(:mktmpdir).and_return("/tmp/foo/bar")
       allow(File).to receive(:read).and_call_original
-      allow(File).to receive(:read).with("foo/bar/xcodebuild.log").and_return('')
+      allow(File).to receive(:read).with("/tmp/foo/bar/xcodebuild.log").and_return('')
       Fastlane::Actions::CompilationTimeProfilerAction.run(
         scheme: "ExampleProject", build_configuration: "Debug",
          workspace: "#{fixture}/ExampleWorkspace.xcworkspace",
