fd3c56065bd924ce0c943b12c17d3ad7f6b559b4
Yuta Saito <kateinoigakukun@gmail.com>
2019-06-11 23:05:12 +0900

    Use framework

---
+++ spec/fixtures/ExampleProject/ExampleProject/ExampleProject.h
@@ -0,0 +1,19 @@
+//
+//  ExampleProject.h
+//  ExampleProject
+//
+//  Created by kateinoigakukun on 2019/06/11.
+//  Copyright © 2019 katei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExampleProject.
+FOUNDATION_EXPORT double ExampleProjectVersionNumber;
+
+//! Project version string for ExampleProject.
+FOUNDATION_EXPORT const unsigned char ExampleProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleProject/PublicHeader.h>
+
+
