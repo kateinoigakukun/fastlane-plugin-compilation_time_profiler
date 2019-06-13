3bed572f984fd5cde627a3ad1c800d02981b2281
Yuta Saito <kateinoigakukun@gmail.com>
2019-06-13 19:47:52 +0900

    Add other project fixture

---
+++ spec/fixtures/OtherProject/OtherProject/OtherProject.h
@@ -0,0 +1,19 @@
+//
+//  OtherProject.h
+//  OtherProject
+//
+//  Created by kateinoigakukun on 2019/06/13.
+//  Copyright © 2019 katei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OtherProject.
+FOUNDATION_EXPORT double OtherProjectVersionNumber;
+
+//! Project version string for OtherProject.
+FOUNDATION_EXPORT const unsigned char OtherProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OtherProject/PublicHeader.h>
+
+
